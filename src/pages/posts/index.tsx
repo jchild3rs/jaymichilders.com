8a30e9db9ab3aeaa3b35556cbc38b70e2820dc6f
James Childers <jchilders62@gmail.com>
2020-05-10 23:27:58 -0400

    use router

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../../components/Header";
 
 export default ({ posts }) => (
@@ -9,12 +10,16 @@ export default ({ posts }) => (
     </Head>
     <ul className="divide-y divide-white divide-opacity-25 max-w-xs container mx-auto">
       <li className="py-4">
-        <a
-          href="/posts/styled-system-styled-components-vs-tailwind-for-react-apps"
-          title="Styled System + Styled Components vs. Tailwind for React apps"
-        >
-          <h2>Styled System + Styled Components vs. Tailwind for React apps</h2>
-        </a>
+        <Link href="/posts/styled-system-styled-components-vs-tailwind-for-react-apps">
+          <a
+            href="/posts/styled-system-styled-components-vs-tailwind-for-react-apps"
+            title="Styled System + Styled Components vs. Tailwind for React apps"
+          >
+            <h2>
+              Styled System + Styled Components vs. Tailwind for React apps
+            </h2>
+          </a>
+        </Link>
         <div className="text-right text-sm text-white text-opacity-75 light-mode:text-black light-mode:text-opacity-50">
           - May 5th, 2020 10:56PM
         </div>
