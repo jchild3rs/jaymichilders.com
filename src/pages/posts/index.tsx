fd0a34e7eac2a88fab428969b0b5c2ec984b70ad
James Childers <jchilders62@gmail.com>
2020-05-10 23:26:58 -0400

    remove fs stuff

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,33 +1,24 @@
-import { format } from "date-fns";
-import Link from "next/link";
+import Head from "next/head";
 import Header from "../../components/Header";
-import readPosts from "../../utils/readPosts";
 
 export default ({ posts }) => (
   <>
     <Header className="my-8" />
-
+    <Head>
+      <title>Posts | Jaymi Childers</title>
+    </Head>
     <ul className="divide-y divide-white divide-opacity-25 max-w-xs container mx-auto">
-      {posts.map((post, i) => (
-        <li key={i} className="py-4">
-          <Link href={post.url}>
-            <a href={post.url} title={post.title}>
-              <h2 className="capitalize underline">{post.title}</h2>
-            </a>
-          </Link>
-          <div className="text-right text-sm text-white text-opacity-75 light-mode:text-black light-mode:text-opacity-50">
-            -{" "}
-            {format(
-              new Date(parseInt(post.lastModified)),
-              "MMMM Mo, yyyy h:mb"
-            )}
-          </div>
-        </li>
-      ))}
+      <li className="py-4">
+        <a
+          href="/posts/styled-system-styled-components-vs-tailwind-for-react-apps"
+          title="Styled System + Styled Components vs. Tailwind for React apps"
+        >
+          <h2>Styled System + Styled Components vs. Tailwind for React apps</h2>
+        </a>
+        <div className="text-right text-sm text-white text-opacity-75 light-mode:text-black light-mode:text-opacity-50">
+          - May 5th, 2020 10:56PM
+        </div>
+      </li>
     </ul>
   </>
 );
-
-export async function getServerSideProps(_context) {
-  return { props: { posts: await readPosts() } };
-}
